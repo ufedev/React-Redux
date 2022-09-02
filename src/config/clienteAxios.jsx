e58cea2ab70a66f83fdc5df7ed6c07993e8fc3e4
Federico Malfasi <Federico.malfasi@gmail.com>
2022-09-02 18:53:58 -0300

    Update clienteAxios.jsx

--- src/config/clienteAxios.jsx
+++ src/config/clienteAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const clienteAxios = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://my-json-server.typicode.com/ufedev/React-Redux",
 })
 
 export default clienteAxios
